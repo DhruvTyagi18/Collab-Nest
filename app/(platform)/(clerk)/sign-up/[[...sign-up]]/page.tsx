98a1fb7fce5579ba94b2802c027bc182f9c74b44
DhruvTyagi18 <92265404+DhruvTyagi18@users.noreply.github.com>
2024-09-26 20:07:23 +0530

    Update page.tsx

--- app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
+++ app/(platform)/(clerk)/sign-up/[[...sign-up]]/page.tsx
@@ -4,42 +4,4 @@ export default function Page() {
   return <SignUp />
 }
 
-import { SignUp } from '@clerk/nextjs'
 
-export default function Page() {
-  return <SignUp />
-}
-// "use client"; 
-// import { useState } from 'react';
-// import { SignUp } from '@clerk/nextjs';
-// import ReCAPTCHA from 'react-google-recaptcha';
-
-// export default function Page() {
-//   const [captchaVerified, setCaptchaVerified] = useState(false);
-
-//   const onCaptchaChange = (value: string | null) => {
-//     if (value) {
-//       setCaptchaVerified(true);  // CAPTCHA passed
-//     } else {
-//       setCaptchaVerified(false); // CAPTCHA failed or reset
-//     }
-//   };
-  
-
-//   return (
-//     <div>
-//       {/* CAPTCHA */}
-//       <ReCAPTCHA
-//         sitekey="6LdU0U8qAAAAAHvzYzZZzUygaRAmkQiFY_zgit-5"  // Replace with your Google reCAPTCHA site key
-//         onChange={onCaptchaChange}
-//       />
-
-//       {/* Clerk Sign Up - only visible if CAPTCHA is verified */}
-//       {captchaVerified ? (
-//         <SignUp />
-//       ) : (
-//         <p>Please complete the CAPTCHA to sign up.</p>
-//       )}
-//     </div>
-//   );
-// }
