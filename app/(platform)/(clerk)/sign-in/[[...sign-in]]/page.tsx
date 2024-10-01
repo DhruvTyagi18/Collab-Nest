bdf147f4bd1a3c5c7a1617ce84cb4fa567c7c6e6
DhruvTyagi18 <92265404+DhruvTyagi18@users.noreply.github.com>
2024-10-01 17:38:05 +0530

    Update page.tsx

--- app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
+++ app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -1,39 +1,35 @@
-import { SignIn } from '@clerk/nextjs'
+
+"use client"; 
+import { useState } from 'react';
+import { SignIn } from '@clerk/nextjs';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function Page() {
-  return <SignIn />
-}
-// "use client"; 
-// import { useState } from 'react';
-// import { SignIn } from '@clerk/nextjs';
-// import ReCAPTCHA from 'react-google-recaptcha';
+  const [captchaVerified, setCaptchaVerified] = useState(false);
 
-// export default function Page() {
-//   const [captchaVerified, setCaptchaVerified] = useState(false);
-
-//   const onCaptchaChange = (value: string | null) => {
-//     if (value) {
-//       setCaptchaVerified(true);  // CAPTCHA passed
-//     } else {
-//       setCaptchaVerified(false); // CAPTCHA failed or reset
-//     }
-//   };
+  const onCaptchaChange = (value: string | null) => {
+    if (value) {
+      setCaptchaVerified(true);  // CAPTCHA passed
+    } else {
+      setCaptchaVerified(false); // CAPTCHA failed or reset
+    }
+  };
   
 
-//   return (
-//     <div>
-//       {/* CAPTCHA */}
-//       <ReCAPTCHA
-//         sitekey="6LcNlVIqAAAAAMERqPDMqxdDXr2dzxXywGnNWLW4"  // Replace with your Google reCAPTCHA site key
-//         onChange={onCaptchaChange}
-//       />
+  return (
+    <div>
+      {/* CAPTCHA */}
+      <ReCAPTCHA
+        sitekey="6LcNlVIqAAAAAMERqPDMqxdDXr2dzxXywGnNWLW4"  // Replace with your Google reCAPTCHA site key
+        onChange={onCaptchaChange}
+      />
 
-//       {/* Clerk Sign Up - only visible if CAPTCHA is verified */}
-//       {captchaVerified ? (
-//         <SignIn />
-//       ) : (
-//         <p>Please complete the CAPTCHA to sign in.</p>
-//       )}
-//     </div>
-//   );
-// }
+      {/* Clerk Sign Up - only visible if CAPTCHA is verified */}
+      {captchaVerified ? (
+        <SignIn />
+      ) : (
+        <p>Please complete the CAPTCHA to sign in.</p>
+      )}
+    </div>
+  );
+}
